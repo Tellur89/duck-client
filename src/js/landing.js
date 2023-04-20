4fe34b4c98b6557dcdef7c3e42d4f820754a3b02
Beckibuzz93 <beckilewis1@hotmail.com>
2023-04-20 12:19:12 +0100

    add function to fetch data, add watchify and concurrentlyto devdep

---
+++ src/js/landing.js
@@ -0,0 +1,32 @@
+const url = 'http://localhost:3000/questions/';
+
+fetch(url)
+.then(resp => resp.json())
+.then(data => 
+    {
+        for(const dataCats in data)
+        {
+            console.log(dataCats)
+        }
+    })
+.catch(err => console.log(err));
+
+function getCategories()
+{
+
+}
+
+getCategories();
+
+/* 
+<div class="col">
+	<section class="georgraphy-section">
+		<a href=#>
+			<div class="button">
+				<h6>Geography</h6>
+				<img src="./img/icons/earthIcon.png" alt="Earth icon">
+			</div>
+		</a>
+	</section>
+</div>
+*/
